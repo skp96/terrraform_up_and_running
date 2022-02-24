e3d240232afc96b58a03b6902c9e41e01d325c10
skp96 <patel.sunny293@gmail.com>
2022-02-24 10:38:25 -0500

    partial backend configuration

--- S3/main.tf
+++ S3/main.tf
@@ -2,12 +2,7 @@ terraform {
     required_version = ">= 0.12, < 0.13" 
 
     backend "s3" {
-        bucket = "terraform-up-and-running-state-skp"
         key = "global/s3/terraform.tfstate"
-        region = "us-east-2"
-
-        dynamodb_table = "terraform-up-and-running-locks"
-        encrypt = true
     } 
 }
 
